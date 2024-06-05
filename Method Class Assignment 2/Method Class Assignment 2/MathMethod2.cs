13f6719814804aef3631bddbede2cdbe5ba41a76
bpitme <128807311+bpitme@users.noreply.github.com>
2024-06-05 03:19:11 +0100

    Hi This is  Polymorphism Assignment

---
+++ Method Class Assignment 2/Method Class Assignment 2/MathMethod2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment_2
+{
+    internal class MathMethod2
+    {
+        //First method takes an integer
+        public int MathOp(int number)
+        {
+            return number + 10;
+        }
+        // Second method takes a decimal
+        public int MathOp(decimal number)
+        {
+            //convert the decimal to an int to perform math opration
+            int newnum = Convert.ToInt32(number);
+            return newnum * 20;
+        }
+        //Third MathOp method that takes a string 
+        public int MathOp(string number)
+        {
+            //convert the string to an int to perform math opration
+            int newmum = Convert.ToInt32(number);
+            return newmum % 8;
+        }
+    }
+}
+        
+
